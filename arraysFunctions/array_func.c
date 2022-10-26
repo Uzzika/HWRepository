6e82b0f0030c69e009d2d3727b0b2d5536be45b5
Dudchenko Olesya <dudchenkoleska@gmail.com>
2022-10-27 01:32:47 +0300

    добавлена функция нахождения среднего арифметического

--- arraysFunctions/array_func.c
+++ arraysFunctions/array_func.c
@@ -31,24 +31,39 @@ int maxim(int* mass, int size) {
 	}
 	return max;
 }
+int sredn(int* mass, int size) {
+	int sr;
+	for (int i = 0; i < size; i++) {
+		sr = mass[i];
+	}
+	sr = sr / size;
+	return sr;
+	
+}
 int main() {
 	srand(time(0));
 	setlocale(LC_ALL, "rus");
 
 	int* mass;
-	int size, mini, max;
+	int size, mini, max, sred;
 
 	printf("Введите длину массива: ");
 	scanf_s("%d", &size);
 	mass = (int*)malloc(sizeof(int) * size);
 	generateMass(size, mass);
-	printMass(mass, size);
+	
+
 	mini = minim(mass, size);
-	printf("\n%d\n", mini);
+	printf("\nМинимальный член %d\n", mini);
 
 	max = maxim(mass, size);
-	printf("\n%d\n", max);
+	printf("\nМаксимальный член %d\n", max);
 
+	sred = sredn(mass, size);
+	printf("\nСреднее арифметическое %d\n", sred);
+	
+	printMass(mass, size);
+	
 	return 0;
 	system("pause");
 }
