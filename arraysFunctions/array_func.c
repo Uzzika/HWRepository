5f592b395d5cb2061ab72d90756c37df9271412b
Dudchenko Olesya <dudchenkoleska@gmail.com>
2022-10-27 00:40:30 +0300

    добавлена функция с нахождением минимального элемента массива

--- arraysFunctions/array_func.c
+++ arraysFunctions/array_func.c
@@ -13,19 +13,29 @@ void printMass(int* mass, int size) {
 		printf("%d ", mass[i]);
 	}
 }
-
+int minim(int* mass, int size) {
+	int minimal = 100000;
+	for (int i = 0; i < size; i++) {
+		if (mass[i] < minimal) {
+			minimal = mass[i];
+		}
+	}
+	return minimal;
+}
 int main() {
 	srand(time(0));
 	setlocale(LC_ALL, "rus");
 
 	int* mass;
-	int size;
+	int size, mini;
 
 	printf("¬ведите длину массива: ");
 	scanf_s("%d", &size);
 	mass = (int*)malloc(sizeof(int) * size);
 	generateMass(size, mass);
 	printMass(mass, size);
+	mini = minim(mass, size);
+	printf("\n%d\n", mini);
 
 	return 0;
 	system("pause");
