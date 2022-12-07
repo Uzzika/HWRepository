3cbbbdf9b7fe3e6a01c49ec865fe5521ce2e0683
Dudchenko Olesya <dudchenkoleska@gmail.com>
2022-12-08 02:38:31 +0300

    финальная версия

--- HW_structure/structureTask1.c
+++ HW_structure/structureTask1.c
@@ -64,7 +64,51 @@ struct TV
 	int width;
 	int weight;
 };
+struct f_member {
+	int age;
+	char position, name;
+};
 
+void input_bookshop(struct book_shop* abob) {
+	printf("Input year of release: ");
+	scanf_s("%d", &abob->year);
+	printf("Input number of pages: ");
+	scanf_s("%d", &abob->c_str); 
+	printf("Input article number: ");
+	scanf_s("%d", &abob->art);
+	printf("Input author's name: ");
+	scanf_s("%c", &abob->author);
+	printf("Input title of the book: ");
+	scanf_s("%c", &abob->title);
+	printf("Input name of the publishing house: ");
+	scanf_s("%c", &abob->publish);
+}
+void input_fio(struct fio* names) {
+	printf("Input name: ");
+	scanf_s("%c", &names->name);
+	printf("Input surname: ");
+	scanf_s("%c", &names->surname);
+	printf("Input patronymic: ");
+	scanf_s("%c", &names->patr);
+}
+void input_telek(struct telek* dim) {
+	printf("Input height: ");
+	scanf_s("%f", &dim->height);
+	printf("Input width: ");
+	scanf_s("%f", &dim->width);
+	printf("Input length: ");
+	scanf_s("%f", &dim->length);
+	printf("Input TV manufacturer: ");
+	scanf_s("%c", &dim->manufac);
+}
+void input_time(struct time* chto) {
+	printf("Input hour: ");
+	scanf_s("%d", &chto->hour);
+	printf("Input minute: ");
+	scanf_s("%d", &chto->minute);
+	printf("Input second: ");
+	scanf_s("%d", &chto->second);
+}
 
 int main() {
 	struct dimensions obj = { 3, 2, 4 };
@@ -78,96 +122,110 @@ int main() {
 	struct body_parameters chechick = { 102, 87, 106, 180 };
 	struct worker ne_ya = { {"Dmitriy", "Hovanski", "Dyrak"}, 7, 9, 2018, 10970, 919092, "Ingeneer" };
 
-	struct dimensions* bs[100];
-	struct dimensions first = { 3, 5, 4 }, second = { 3, 4, 1 }, last = { 9, 4, 2 };
+	struct dish* bs[100]; // рецепт
+	struct dish first = {"pasta", "boloneze", {3, 5, 4, 800.0}}, second = { "pastaA", "carbonara", {45, 2, 23, 1200.0} }, last = { "pastaaa", "orzo", {34, 12, 5, 950.0} };
 
-	bs[0]->height = first.height;
-	bs[0]->width = first.width;
-	bs[0]->length = first.length;
+	bs[0]->ingredients = first.ingredients;
+	bs[0]->title = first.title;
+	bs[0]->pasta = first.pasta;
 
-	bs[0]->height = second.height;
-	bs[0]->width = second.width;
-	bs[0]->length = second.length;
+	bs[1]->ingredients = second.ingredients;
+	bs[1]->title = second.title;
+	bs[1]->pasta = second.pasta;
 
-	bs[0]->height = last.height;
-	bs[0]->width = last.width;
-	bs[0]->length = last.length;
+	bs[2]->ingredients = last.ingredients;
+	bs[2]->title = last.title;
+	bs[2]->pasta = last.pasta;
+
+	struct f_member* mem[100]; // семь€
+	struct f_member fir = { 22, "mother", "Mary" }, sec = { 24, "father", "John"}, las = { 1, "daughter", "Missi"};
+
+	mem[0]->age = fir.age;
+	mem[0]->position = fir.position;
+	mem[0]->name = fir.name;
+
+	mem[1]->age = sec.age;
+	mem[1]->position = sec.position;
+	mem[1]->name = sec.name;
+
+	mem[2]->age = las.age;
+	mem[2]->position = las.position;
+	mem[2]->name = las.name;
 	
-	struct TV* size;
-	Input(&size);
-	diagonal(&size);
-	square(&size);
-	volume(&size);
-
 	return 0;
 
 }
-struct Dimension { // шкаф (описываетс€ габаритами)
-	int height;
-	int depth;
-	int width;
+
+struct Dimension {
+	int height, depth, width;
 };
-// ввод параметров шкафа
+
 void input(struct Dimension* sizes) {
-	printf("Enter cabinet width: ");
-	scanf_s("%d", &(*sizes).width);
-	printf("Enter cabinet height: ");
-	scanf_s("%d", &(*sizes).height);
-	printf("Enter cabinet depth : ");
-	scanf_s("%d", &(*sizes).depth);
+	printf("Input cabinet width: ");
+	scanf_s("%d", &sizes->width);
+	printf("Input cabinet height: ");
+	scanf_s("%d", &sizes->height);
+	printf("Input cabinet depth : ");
+	scanf_s("%d", &sizes->depth);
 }
-// расчЄт поверхности шкафа
+
 int calculateSquare(struct Dimension sizes) {
-	int square_bok, square_updown, square_front, square;
-	// 2 боковые крышки
-	square_bok = 2 * sizes.height * sizes.depth;
-	// верхн€€ и нижн€€ крышки
+	int square_bokovie, square_updown, square_front, square;
+
+	square_bokovie = 2 * sizes.height * sizes.depth;
+
 	square_updown = 2 * sizes.width * sizes.depth;
-	// 1 передн€€ крышка
+
 	square_front = sizes.height * sizes.width;
-	square = square_bok + square_updown + square_front;
+	square = square_bokovie + square_updown + square_front;
+	
 	return square;
 }
-// расчЄт объЄма шкафа
+
 int calculateVolume(struct Dimension sizes) {
 	int V = sizes.height * sizes.depth * sizes.width;
 	return V;
 }
-// расчЄт сколько краски нужно на весь шкаф
-// consumption Ц расход краски на м2
+
 float calculateDye(struct Dimension sizes, float consumption) {
-	// с помощью написанной ранее функции вычисл€ем площадь поверхности
+
 	int square = calculateSquare(sizes);
-	// зна€ расход и площадь вычисл€ем сколько краски нужно
-	float dye_needed = square * consumption;
+
+	float dye_needed = square * consumption; 
 	return dye_needed;
 }
+
 float calculatePaintingCost(struct Dimension sizes, float consumption, float
 	workCost, float paintCost) {
-	// ваш код
+
 	float paint_price = calculateDye(sizes, consumption) * paintCost;
 	float work_price = calculateDye(sizes, consumption) * workCost;
 	float painting_cost = paint_price + work_price;
 	return painting_cost;
 }
+
 float calculateCupboardCost(struct Dimension sizes, float cupboardCost) {
 	float cabinet_cost = calculateSquare(sizes) * cupboardCost;
 	return cabinet_cost;
 }
+
 float calculateCabinetMass(struct Dimension sizes, float weight) {
 	float cabinet_mass = calculateSquare(sizes) * weight;
 	return cabinet_mass;
 }
+
 float calculateLiftingCost(struct Dimension sizes, float weight, int floorsCount, float riseCost) {
 	float LiftingCost = calculateCabinetMass(sizes, weight) * floorsCount * riseCost;
 	return LiftingCost;
 }
+
 float calculateCost(struct Dimension sizes, float cupboardCost, float consumption, float paintCost,
 	float workCost, float weight, int floorsCount, float riseCost, float deliveryCost) {
 	float full_cost = calculatePaintingCost(sizes, consumption, workCost, paintCost) +
 		calculateCupboardCost(sizes, cupboardCost) + calculateLiftingCost(sizes, weight, floorsCount, riseCost) + deliveryCost;
 	return full_cost;
 }
+
 int main() {
 	// расход краски, стоимость работы по покраске, стоимость краски
 	float paintConsumption = 0.8, paintWorkCost = 35.00, paintCost = 50.00;
